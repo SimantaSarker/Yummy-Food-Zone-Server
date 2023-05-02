97867cd9fc7af6146582eeef9711b22deb53943c
SimantaSarker <simantasarker0000@gmail.com>
2023-05-02 13:59:59 +0600

    chef data showing in the port

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const chef=require('./data/chefs.json')
 const port = process.env.PORT || 4000;
 
 app.use(cors());
@@ -8,6 +9,17 @@ app.use(cors());
 app.get('/',(req,res)=>{
 res.send("Api is running")
 })
+
+app.get('/chefs',(req,res)=>{
+  res.send(chef);
+})
+
+
+app.listen(port,()=>{
+  console.log(`Dragon API is running not on ${port}`);
+})
+
+
 
 
 
